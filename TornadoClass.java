2d3d05ef90af003ea31b8a9c1268655c7beeb007
Susan H <39858243+ProgrammerSusan@users.noreply.github.com>
2019-01-15 09:50:07 -0500

    Add files via upload

---
+++ TornadoClass.java
@@ -0,0 +1,14 @@
+
+/*
+ * Susan Hatem
+ * Code help from: https://www.youtube.com/playlist?list=PLSmcJ5vtyZR7hB-mIAsJMhoiQV4DyCE9v
+ * https://github.com/ProgrammerSusan/Tornado
+ */
+
+public class TornadoClass {
+    public static void main(String[] args) {
+        //calls the simulation and sets speed of simulation
+        Tornado tornado = new Tornado();
+        tornado.simulationTime(500);
+    }
+}
